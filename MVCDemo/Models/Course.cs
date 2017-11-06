4a6020f9e220619582b525286803d35b3ca98a87
sharath <sharathc443@gmail.com>
2017-11-05 21:31:49 -0600

    Initial Commit

---
+++ MVCDemo/Models/Course.cs
@@ -0,0 +1,11 @@
+﻿namespace MVCDemo.Models
+{
+    public class Course
+    {
+        public int CourseID { get; set; }
+        public string Name { get; set; }
+        public int Capacity { get; set; }
+
+
+    }
+}
---
+++ MVCDemo/Models/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MVCDemo.Models
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public int StudentID { get; set; }
+        public string Address { get; set; }
+        public string Contact { get; set; }
+        public DateTime DateOfBirth { get; set; }
+
+
+
+    }
+}
