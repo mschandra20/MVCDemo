a0f12c84fa965808bdbf1edd0163878113be41da
sharath <sharathc443@gmail.com>
2017-11-06 15:34:13 -0600

    Constructor and connection string included

--- MVCDemo/Models/StudentContext.cs
+++ MVCDemo/Models/StudentContext.cs
@@ -4,8 +4,20 @@ namespace MVCDemo.Models
 {
     public class StudentContext:DbContext
     {
-       public DbSet<Course> DbSetCourses { get; set; }
+        public StudentContext() : base("StudentCourseDatabase")
+        {
+
+        }
+        public DbSet<Course> DbSetCourses { get; set; }
         public DbSet<Student> DbSetStudents { get; set; }
 
+
+
+        //FLUENT API to override the conventions
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
