05c83170565ea12d7fbc77d3d053e8b3ced96b29
sharath <sharathc443@gmail.com>
2017-11-06 22:34:18 -0600

    Added StudentController to display current students and  add new students

---
+++ MVCDemo/Controllers/StudentController.cs
@@ -0,0 +1,107 @@
+﻿using MVCDemo.Models;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace MVCDemo.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            StudentContext s_context = new StudentContext();
+            var ListOfStudents= s_context.DbSetStudents.ToList();
+
+            return View(ListOfStudents);
+        }
+
+        // GET: Student/Details/5
+        public ActionResult Details(int id)
+        {
+
+
+            return View();
+        }
+
+        // GET: Student/Create
+        [HttpGet]
+        public ActionResult Create()
+        {
+
+
+            return View();
+        }
+
+        // POST: Student/Create
+        [HttpPost]
+        public ActionResult Create(Student student)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                StudentContext s_context = new StudentContext();
+                s_context.DbSetStudents.Add(
+                new Student
+                {
+                    Name=student.Name,
+                    Address=student.Address,
+                    Contact=student.Contact,
+                    DateOfBirth=student.DateOfBirth
+                });
+
+
+                s_context.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Student/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Student/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Student/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Student/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
