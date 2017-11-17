81946176f1b898cf4382fa68802362bd520227e8
sharath <sharathc443@gmail.com>
2017-11-16 23:38:00 -0600

    EnrollController edited but need to add functionality

--- MVCDemo/Controllers/EnrollController.cs
+++ MVCDemo/Controllers/EnrollController.cs
@@ -1,4 +1,6 @@
 ﻿using MVCDemo.Models;
+using System;
+using System.Linq;
 using System.Web.Mvc;
 
 namespace MVCDemo.Controllers
@@ -21,16 +23,24 @@ namespace MVCDemo.Controllers
         }
 
         [HttpPost]
-        public ActionResult Enroll(Enrollment enroll)
+        public ActionResult Enroll(FormCollection enroll)
         {
             MContext s_context = new MContext();
+            var t = Convert.ToInt32(Request.Form["Course Number"]);
+            var u = Convert.ToInt32(Request.Form["Enrollement Number"]);
+            
+            var s = s_context.DbSetStudents.First(x=>x.StudentID == u);
+            var c = s_context.DbSetCourses.First(x => x.CourseNumber == t);
+
             s_context.DbSetEnrollments.Add(
                 new Enrollment
                 {
-                    CourseID=enroll.CourseID,
-                    StudentID=enroll.StudentID
+                    CourseID=c.CourseID,
+                    StudentID=s.StudentID
 
                 });
+            
+            s_context.SaveChanges();
             return RedirectToAction("Index","Student");
         }
     }
