d9c39e5227556b5045013edfc97b46f1cd21dde9
sharath <sharathc443@gmail.com>
2017-11-22 12:11:09 -0600

    MAde some changes to enroll form

--- MVCDemo/Controllers/EnrollController.cs
+++ MVCDemo/Controllers/EnrollController.cs
@@ -9,13 +9,13 @@ namespace MVCDemo.Controllers
     {
         // GET: Enroll
         [HttpGet]
-        public ActionResult Enroll ()
+        public ActionResult Enroll(FormCollection enroll)
         {
             //Course Name List
             MContext s_context = new MContext();
-            var CourseNameList = s_context.DbSetCourses.ToList();
-            SelectList CNamelist = new SelectList(CourseNameList, "CourseID", "Name");
-            ViewBag.CList = CNamelist;
+            //var CourseNameList = s_context.DbSetCourses.ToList();
+            //SelectList CNamelist = new SelectList(CourseNameList, "CourseID", "Name");
+            //ViewBag.CList = CNamelist;
 
             //Course Number List
             var CourseNumberList = s_context.DbSetCourses.ToList();
@@ -25,38 +25,56 @@ namespace MVCDemo.Controllers
             //var s = s_context.DbSetEnrollments.FirstOrDefault(i => i.student.StudentID == id);
 
             //Student Name List
-            var StudentNameList = s_context.DbSetStudents.ToList();
-            SelectList SNamelist = new SelectList(StudentNameList, "StudentID", "Name");
-            ViewBag.SList = SNamelist;
+            //var StudentNameList = s_context.DbSetStudents.ToList();
+            //SelectList SNamelist = new SelectList(StudentNameList, "StudentID", "Name");
+            //ViewBag.SList = SNamelist;
 
             //Student Number List
             var StudentNumberList = s_context.DbSetStudents.ToList();
             SelectList SNumlist = new SelectList(StudentNumberList, "StudentID", "EnrollmentNumber");
             ViewBag.SNList = SNumlist;
 
-            return View();
+            var s = GetStudent(enroll);
+            Enrollment e = new Enrollment();
+            e.student = s;
+            return View(e);
         }
 
         [HttpPost]
-        public ActionResult Enroll(FormCollection enroll)
+        [ActionName("Enroll")]
+        public ActionResult Enroll_post(FormCollection enroll)
         {
             MContext s_context = new MContext();
             var t = Convert.ToInt32(Request.Form["Course Number"]);
             var u = Convert.ToInt32(Request.Form["Enrollement Number"]);
-            
-            var s = s_context.DbSetStudents.First(x=>x.StudentID == u);
+
+            var s = s_context.DbSetStudents.First(x => x.EnrollmentNumber == u);
             var c = s_context.DbSetCourses.First(x => x.CourseNumber == t);
 
             s_context.DbSetEnrollments.Add(
                 new Enrollment
                 {
-                    CourseID=c.CourseID,
-                    StudentID=s.StudentID
+                    CourseID = c.CourseID,
+                    StudentID = s.StudentID
 
                 });
-            
+
             s_context.SaveChanges();
-            return RedirectToAction("Index","Student");
+            return RedirectToAction("Index", "Student");
         }
+
+        
+        public Student GetStudent(FormCollection enroll)
+        {
+            MContext s_context = new MContext();
+            var sn  = Convert.ToInt32(Request.Form["Enrollment Number"]);
+            var sobj = s_context.DbSetStudents.First(x => x.EnrollmentNumber == sn);
+            var sid = sobj.StudentID;
+
+
+            return sobj;
+        }
+
+
     }
 }
